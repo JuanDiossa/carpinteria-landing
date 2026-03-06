d851fe7e0e7f63b756602a3093702dba9dab4995
juandiossa <djuan.d.o@gmail.com>
2026-03-05 21:45:29 -0500

    fix: update Gallery component titles and improve layout for better readability

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -61,12 +61,12 @@ export function Gallery() {
   return (
     <section className="w-full overflow-hidden bg-background-light py-16">
       <div className="container max-w-9xl mx-auto flex items-center justify-between">
-        <div>
-          <span className="text-primary font-bold uppercase tracking-wider px-5">
+        <div className="flex flex-col items-start space-y-2">
+          <span className=" text-primary text-xs font-bold uppercase tracking-wider px-5">
             TRABAJOS SELECCIONADOS
           </span>
           <h2 className="text-2xl md:text-4xl sm:text-3xl font-black leading-[1.1] tracking-tight px-5">
-            GALERIA DE TRABAJOS
+            GALERIA
           </h2>
         </div>
         <nav className="border border-gray-200 rounded-lg mr-5">
@@ -74,7 +74,7 @@ export function Gallery() {
             {categories.map((category) => (
               <li
                 key={category}
-                className={`text-sm font-medium py-2 px-4 rounded-xl transition-all duration-300 cursor-pointer
+                className={`sm:text-sm text-xs font-medium py-1 px-2 sm:py-2 sm:px-4 rounded-xl transition-all duration-300 cursor-pointer
                   ${
                     selectedCategory === category
                       ? "bg-white text-charcoal shadow-sm"
